03ab5b47c647b040bb8a285cd2efd9e5867aae63
Brian Simiyu <briansimiyuj@gmail.com>
2022-12-31 08:33:39 +0300

    Skip an element

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const alphabets = ['A', 'B', 'C', 'D', 'E', 'F']
 
 const numbers = ['1', '2', '3', '4', '5', '6']
 
-const [ a, b, c ] = alphabets
+const [ a, , c ] = alphabets
 
 console.log(a)
-
-console.log(b)
 
 console.log(c)
