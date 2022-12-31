baa8e0dbacba7c41b923379bfdb6acde3998b99f
Brian Simiyu <briansimiyuj@gmail.com>
2022-12-31 08:36:45 +0300

    Spread Operator

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const alphabets = ['A', 'B', 'C', 'D', 'E', 'F']
 
 const numbers = ['1', '2', '3', '4', '5', '6']
 
-const [ a, , c ] = alphabets
+const [ a, , c, ...rest ] = alphabets
 
 console.log(a)
 
 console.log(c)
+
+console.log(rest)
