e0dc20c6a64448ece11d0967856919f8738b29bf
Brian Simiyu <briansimiyuj@gmail.com>
2022-12-31 08:58:36 +0300

    Destructuring function 2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function sumAndMultiply(a, b){
 
-   return [a + b, a *b]
+   return [a + b, a * b, a / b]
 
 }
 
-const [ sum, multiply ] = sumAndMultiply(2, 3)
+const [ sum, multiply, division = 'No Division' ] = sumAndMultiply(2, 3)
 
 console.log(sum)
 
 console.log(multiply)
+
+console.log(division)
