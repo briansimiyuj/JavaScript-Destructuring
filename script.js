1fdf2d291970856d2a82f508b9e9ccee7a6e7c9f
Brian Simiyu <briansimiyuj@gmail.com>
2022-12-31 08:24:32 +0300

    Old destructuring way

---
+++ script.js
@@ -0,0 +1,11 @@
+const alphabets = ['A', 'B', 'C', 'D', 'E', 'F']
+
+const numbers = ['1', '2', '3', '4', '5', '6']
+
+const a = alphabets[0]
+
+const b = alphabets[3]
+
+console.log(a)
+
+console.log(b)
